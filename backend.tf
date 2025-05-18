6df49c89b2f0e55b203d62677ed93b7dfc530d59
epublic9 <epublic786@gmail.com>
2025-05-18 19:30:56 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,7 @@
 terraform {
   backend "gcs" {
     bucket  = "bucket-usdev-samsung-statefiles-str01"       # Replace with your bucket name
-    prefix  = "projects/${var.project_id/region}"          # Logical folder path
+    #prefix  = "projects/${var.project_id/region}"          # Variables may not be used here.
+     prefix  = "projects/pid-usdev-atoberg-s24-res01/us-east1"
   }
 }
