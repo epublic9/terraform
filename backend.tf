faba1d53c72da433b43c81221da642e5d998a90e
epublic9 <epublic786@gmail.com>
2025-05-18 19:27:36 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
     bucket  = "bucket-usdev-samsung-statefiles-str01"       # Replace with your bucket name
-    prefix  = "projects/${var.project_id/region}}"          # Logical folder path
+    prefix  = "projects/${var.project_id/region}"          # Logical folder path
   }
 }
