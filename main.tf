c4e10accd9a55520f88de77340dadc05f589b93f
epublic9 <epublic786@gmail.com>
2025-05-24 15:16:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,9 @@ resource "google_project" "project" {
   billing_account = var.billing_account_id
 }
 
+resource "google_project_service" "services" {
+  for_each = toset(var.api_services)
+  project  = google_project.project.project_id
+  service  = each.key
+  disable_on_destroy = false
+}
