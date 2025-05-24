f7e70884be5780e241655396afbc5081b47c8429
epublic9 <epublic786@gmail.com>
2025-05-24 15:04:35 +0530

    Update service-account.tf

--- service-account.tf
+++ service-account.tf
@@ -5,4 +5,9 @@ resource "google_service_account" "appengine_sa_account" {
   description = var.service_account_description
 }
 
-
+resource "google_project_iam_member" "appengine_sa_account_iam_member" {
+  for_each = toset(var.appengine_sa_roles)
+   project = var.project_id
+   role = each.value
+   member = "serviceAccount:${google_service_account.appengine_sa_account.email}"
+}
